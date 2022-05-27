8e4778f057298730bb0d1bbffa52dc6d34f43ee5
Piyush Pansuriya <piyush@ostechlabs.com>
2022-05-27 18:24:37 +0530

    company changes

--- OmniCRM_Web/Controllers/CallDetailsController.cs
+++ OmniCRM_Web/Controllers/CallDetailsController.cs
@@ -709,7 +709,7 @@ namespace OmniCRM_Web.Controllers
                 //}
 
 
-                listManager = (from user in await _context.UserMaster.Where(p => p.RoleId == (int)Roles.RelationshipManager && p.CompanyId == currentCompanyId).ToListAsync()
+                listManager = (from user in await _context.UserMaster.Where(p => p.RoleId == (int)Roles.RelationshipManager && p.CompanyId == currentCompanyId && p.Status == true).ToListAsync()
                                select user).Select(p => new RMangerViewModel() { UserId = p.UserId, FirstName = p.FirstName, LastName = p.LastName }).ToList();
 
                 GenericMethods.Log(LogType.ActivityLog.ToString(), "GetRelationshipManagerList: " + "-get all relationship manager user");
@@ -741,7 +741,7 @@ namespace OmniCRM_Web.Controllers
                 //}
 
 
-                listTeleCaller = (from user in await _context.UserMaster.Where(p => (p.RoleId == (int)Roles.TeleCaller || p.RoleId == (int)Roles.Admin) && p.CompanyId == currentCompanyId).ToListAsync()
+                listTeleCaller = (from user in await _context.UserMaster.Where(p => (p.RoleId == (int)Roles.TeleCaller || p.RoleId == (int)Roles.Admin) && p.CompanyId == currentCompanyId && p.Status == true).ToListAsync()
                                   select user).Select(p => new RMangerViewModel() { UserId = p.UserId, FirstName = p.FirstName, LastName = p.LastName }).ToList();
 
                 GenericMethods.Log(LogType.ActivityLog.ToString(), "GetTeleCallerList: " + "-get all tele caller user");
